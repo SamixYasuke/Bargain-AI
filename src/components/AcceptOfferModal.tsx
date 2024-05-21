eb618cb1106514e3019677c65d05f30ced9246f8
Samuel Adekolu <95057238+SamixYasuke@users.noreply.github.com>
2024-05-21 11:02:16 +0100

    Update AcceptOfferModal.tsx

--- src/components/AcceptOfferModal.tsx
+++ src/components/AcceptOfferModal.tsx
@@ -3,17 +3,35 @@ import acceptOfferModalStyle from "../styles/acceptOfferModal.module.css";
 
 interface AcceptOfferModalProps {
   setAcceptOfferModalIsOpen: (isOpen: boolean) => void;
+  setWinOfferModalIsOpen: (isOpen: boolean) => void;
+  setLoseOfferModalIsOpen: (isOpen: boolean) => void;
+  currentOffer: number;
+  costPrice: number;
 }
 
 const AcceptOfferModal: React.FC<AcceptOfferModalProps> = ({
   setAcceptOfferModalIsOpen,
+  setWinOfferModalIsOpen,
+  setLoseOfferModalIsOpen,
+  currentOffer,
+  costPrice,
 }) => {
+  const handleAcceptOffer = () => {
+    if (currentOffer >= costPrice) {
+      setAcceptOfferModalIsOpen(false);
+      setWinOfferModalIsOpen(true);
+    } else {
+      setAcceptOfferModalIsOpen(false);
+      setLoseOfferModalIsOpen(true);
+    }
+  };
+
   return (
     <section className={acceptOfferModalStyle.acceptOfferModalContainer}>
       <div>
         <h2>Do You Accept This Offer?</h2>
         <div>
-          <button onClick={() => setAcceptOfferModalIsOpen(false)}>Yes</button>
+          <button onClick={handleAcceptOffer}>Yes</button>
           <button onClick={() => setAcceptOfferModalIsOpen(false)}>No</button>
         </div>
       </div>
